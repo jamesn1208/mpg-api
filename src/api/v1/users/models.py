5316704b6cd1e8aef872cb972dd9ce5b834cb99d
James Nash <james-server@notanemail.com>
2026-05-15 15:02:03 +0000

    PyDoc

--- src/api/v1/users/models.py
+++ src/api/v1/users/models.py
@@ -7,10 +7,26 @@ from api.core.exceptions import UnknownAccount
 
 
 def does_username_exist(username: str, session: Session) -> bool:
-    return session.execute(select(Users).where(Users.name == username)).scalar() is not None
+    """
+    Checks if a given username exists in the Users table.
+
+    :param username: Username to check.
+    :param session: Session to check with.
+    :return: (bool) True if username exists, else False.
+    """
+    return session.execute(select(Users).where(Users.name == username)).scalar() is not None  # type: ignore
 
 
 def create_user(username: str, hashed_password: str, token: str, session: Session) -> int:
+    """
+    Adds provided username, password & token to the Users table, and returns the ID of the new user.
+
+    :param username: The username of the new user.
+    :param hashed_password: The hashed password of the new user (argon2id).
+    :param token: The session token for the new user.
+    :param session: The database session to use.
+    :return: (int) The ID of the new user.
+    """
     user = Users(name=username,
                  hash=hashed_password,
                  session_token=token)
@@ -22,7 +38,14 @@ def create_user(username: str, hashed_password: str, token: str, session: Sessio
 
 
 def get_user_by_username(username: str, session: Session) -> Users:
+    """
+    Get an instance of the Users model by username.
+
+    :param username: The username to get info about.
+    :param session: The database session to use.
+    :return: An instance of the Users model.
+    """
     try:
-        return session.execute(select(Users).where(Users.name == username)).scalar_one()
+        return session.execute(select(Users).where(Users.name == username)).scalar_one()  # type: ignore
     except NoResultFound as e:
         raise UnknownAccount from e
